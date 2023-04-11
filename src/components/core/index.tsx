a64452b0088adf28b7912f824568dabeaa8b6b36
Tiến Dũng <phantiendung29111999@gmail.com>
2023-04-11 14:28:17 +0700

    Add hooks

--- src/components/core/index.tsx
+++ src/components/core/index.tsx
@@ -1,22 +1,10 @@
-import React, {useCallback} from 'react'
-import {Await, useMatches} from '@remix-run/react'
-import type {
-	Cart as CartType,
-} from '@shopify/hydrogen/storefront-api-types'
+import React, {useEffect} from 'react'
+import {useCart} from '../../hooks/useCart'
 
 export default function UpPromoteCoreTacking() {
-	const [root]  = useMatches()
-	const handleOnCardLoaded = useCallback((cart: CartType) => {
-		console.log('Uppromote loaded cart', cart?.id)
-	}, [root.data.cartPromise])
-	return (
-		<Await resolve={root.data?.cartPromise}>
-			{
-				(cart) => {
-					handleOnCardLoaded(cart)
-					return  <p>{cart?.totalQuantity || 0}</p>
-				}
-			}
-		</Await>
-	)
+	const cart = useCart()
+	useEffect(() => {
+		console.log('Uppromote loaded cart', cart)
+	}, [cart])
+	return <></>
 }
---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,19 @@
+import {useMatches} from '@remix-run/react'
+import {useEffect, useRef, useState} from 'react'
+import {Cart} from '@shopify/hydrogen/dist/storefront-api-types'
+
+export function useCart() {
+	const [root] = useMatches()
+	const [cart, setCart] = useState<undefined | boolean | null| Cart>()
+	const resolved = useRef(false)
+
+	useEffect(() => {
+		if (resolved.current) return
+		root.data.cartPromise
+			.then(setCart)
+			.catch(() => setCart(null))
+		resolved.current = true
+	}, [root.data.cartPromise, setCart])
+
+	return cart
+}
