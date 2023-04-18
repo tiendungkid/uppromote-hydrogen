9d5791ecb69e3c8617b3b2f73eb76685a5ac28fb
Tiến Dũng <phantiendung29111999@gmail.com>
2023-04-18 14:32:20 +0700

    Update instance

--- src/components/core/index.tsx
+++ src/components/core/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import {Cart} from '@shopify/hydrogen-react'
 
 interface Props {
-    cart?: Cart | null | any
+    cart?: Cart
 }
 
 export default function Uppromote(props: Props) {
-	if (props.cart)
-		console.log('UpPromote loaded cart', props.cart)
+	const { cart } = props
+	if (cart) {
+		console.log('Uppromote loaded card', cart.id)
+	}
+
 	return <></>
 }
