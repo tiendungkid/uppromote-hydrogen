3a0de21acb1efe8ab896c9f86ff97169ac771cd2
Tiến Dũng <phantiendung29111999@gmail.com>
2023-04-27 10:39:11 +0700

    Remove axios

--- src/utils/axios/index.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-import {uppromoteAppConfig} from '../../config/uppromote.app.config'
-
-export const getAxiosInstance = async () => {
-	return axios.create({
-		baseURL: uppromoteAppConfig().vars.appUrl
-	})
-}
